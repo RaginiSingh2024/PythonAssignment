7017074ed1668bae7c5a7e2c31726daba9babe7c
Ragini Singh <raginisingh.sejal@gmail.com>
2025-01-14 23:07:32 +0530

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,14 @@
+## String Slicing
+my_string = "Hello, World!"
+
+# Reverse the string
+reversed_string = my_string[::-1] 
+print("Reversed string:", reversed_string)
+
+# Skip every other letter
+skipped_string = my_string[::2] 
+print("String with every other letter skipped:", skipped_string)
+
+# Extract a substring
+substring = my_string[7:12]  # Extract "World"
+print("Substring:", substring)
